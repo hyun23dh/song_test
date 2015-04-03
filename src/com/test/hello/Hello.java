af7807471cae0054d3a11f5df2652e0519a9e7b3
hyun23dh <hyun23dh@daum.net>
2015-04-03 14:28:33 +0900

    add customer

--- src/com/test/hello/Hello.java
+++ src/com/test/hello/Hello.java
@@ -2,8 +2,7 @@ package com.test.hello;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("test");
-		System.out.println("test1");
+		Customer cus = new Customer("dezang", "test1234", "È²Á¤¿í");
+		System.out.println(cus);
 	}
-
 }
